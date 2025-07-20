a5eda686cbb44fea5dfad1c306d16280cad86133
krlgarcia <krlgarcia@addu.edu.ph>
2025-07-20 23:35:12 +0800

    modified

---
+++ app/index.tsx
@@ -0,0 +1,53 @@
+import { Link, Stack } from "expo-router";
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+import { useCameraPermissions } from "expo-camera";
+
+export default function Home() {
+  const [permission, requestPermission] = useCameraPermissions();
+
+  const isPermissionGranted = Boolean(permission?.granted);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Stack.Screen options={{ title: "Overview", headerShown: false }} />
+      <Text style={styles.title}>Scanner Test</Text>
+      <View style={{ gap: 20 }}>
+        <Pressable onPress={requestPermission}>
+          <Text style={styles.buttonStyle}>Request Permissions</Text>
+        </Pressable>
+        <Link href={"/scanner"} asChild>
+          <Pressable disabled={!isPermissionGranted}>
+            <Text
+              style={[
+                styles.buttonStyle,
+                { opacity: !isPermissionGranted ? 0.5 : 1 },
+              ]}
+            >
+              Scan Textile
+            </Text>
+          </Pressable>
+        </Link>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+    justifyContent: "space-around",
+    paddingVertical: 80,
+  },
+  title: {
+    color: "black",
+    fontSize: 40,
+  },
+  buttonStyle: {
+    color: "#0E7AFE",
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
